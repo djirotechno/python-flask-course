2d55853d84838c7bd432fecb23eab6430616e3b6
djirotechno <djirotech@gmail.com>
2025-03-25 09:26:16 +0000

    flask route and arg

---
+++ flaskproject/lab/server.py
@@ -0,0 +1,24 @@
+# Import the Flask class from the flask module
+from flask import Flask ,make_response
+
+# Create an instance of the Flask class, passing in the name of the current module
+app = Flask(__name__)
+
+# Define a route for the root URL ("/")
+@app.route("/")
+def index():
+    # Function that handles requests to the root URL
+    # Return a plain text response
+    return "hello world"
+
+@app.route("/no_content")
+def no_content():
+    return ({"message":"no content found"},204)
+
+
+@app.route("/exp")
+def index_exp():
+    resp = make_response({"message":"Hello world"})
+    resp.status_code = 200
+
+    return resp 
